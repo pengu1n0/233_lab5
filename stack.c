b0ad6cde185b809d654408d6b5db7a2fb2fd3a86
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-11 14:49:40 +0700

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main ()
+{
+	int i = 0, j,n, s[100], num;
+	char in[100];
+	scanf("%d", &n);
+	scanf(" %[^\n]", in);
+	
+	char *token = strtok(in, " ");
+	for(j=0; j<n; j++) {
+		if(strcmp(token, "+") == 0)
+		{
+			i--;
+			num = s[i-1] + s[i];
+			s[i] = 0;
+			s[i-1] = num;
+		}
