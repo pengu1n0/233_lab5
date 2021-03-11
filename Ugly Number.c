f6d42ca51c46c68dfb4b6bb59a70834fbdb3db50
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-11 15:28:58 +0700

    Create Ugly Number.c

---
+++ Ugly Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j=0,a=0,b[3]={2,3,5},sum;
+	scanf("%d",&n);
+	for(i=1;1;i++)
+	{
+		sum=i;
+		while(j!=3)
+		{
+			if(sum%b[j]==0)
+            {
+                sum/=b[j];
+            }
+            else
+                j++;
+		}
+	}
+	
+		
+}
